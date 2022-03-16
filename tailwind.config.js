0dad13ec538c3d659626ffbba63ef3f4bc7bc722
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-16 11:12:39 +0900

    mod tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
