841c28e4eae5c45b04892a79101f5de09a74f8e3
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-17 23:10:03 +0900

    mod

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,71 +1,70 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import styles from '@/styles/Contents.module.scss';
 
 const Home: NextPage = () => {
   return (
     <>
-      <main className={styles.main}>
-        <div className={styles.main_inner}>
-          <h2 className={styles.page_title}>Works</h2>
-          <ul className={styles.works}>
-            <li className={styles.works_item}>
+      <main className="main">
+        <div className="main_inner">
+          <h2 className="page_title">Works</h2>
+          <ul className="works">
+            <li className="works_item">
               <Link href="/works/a">
                 <a>
                   <h3>Web / 株式会社a</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/b">
                 <a>
                   <h3>Web / 株式会社b</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/c">
                 <a>
                   <h3>Web / 株式会社c</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/d">
                 <a>
                   <h3>Web / 株式会社d</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/e">
                 <a>
                   <h3>Web / 株式会社e</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/f">
                 <a>
                   <h3>Web / 株式会社f</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/g">
                 <a>
                   <h3>Web / 株式会社g</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/h">
                 <a>
                   <h3>Web / 株式会社h</h3>
                 </a>
               </Link>
             </li>
-            <li className={styles.works_item}>
+            <li className="works_item">
               <Link href="/works/i">
                 <a>
                   <h3>Web / 株式会社i</h3>
