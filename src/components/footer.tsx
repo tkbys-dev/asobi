48536015abe4d7756321f0e594f5451e4cb4fe50
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-23 17:43:20 +0900

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
             className="footer_logo"
             width="132"
             height="49"
-            alt="test. inc"
+            alt="test. in"
           />
         </a>
       </Link>
