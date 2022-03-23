87f80ed2907dd8b911416698738344161d2d1b2a
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-23 16:58:25 +0900

    mod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,16 @@ import '@/styles/reset.css';
 import '@/styles/base.scss';
 import '@/styles/style.scss';
 // import 'tailwindcss/tailwind.css';
-// import Layout from '@/components/Layout';
+import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app';
 import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AnimatePresence>
-      {/* <Layout> */}
-      <Component {...pageProps} />
-      {/* </Layout> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AnimatePresence>
   );
 }
