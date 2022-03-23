24d69c0117f598aacb0a23261e75658c2331acc2
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-23 19:03:58 +0900

    mod

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,105 @@
 import { useRouter } from 'next/router';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Link from 'next/link';
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
 
 export default function Layout({ children }: any) {
   const router = useRouter();
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <header className="header">
+        <Link href="/">
+          <a>
+            <img
+              src="/assets/img/common/header-logo.svg"
+              className="header_logo"
+              width="132"
+              height="49"
+              alt="test. inc"
+            />
+          </a>
+        </Link>
+        <nav className="header_nav">
+          <ul className="header_nav_list">
+            <li className="header_nav_list_item">
+              <Link href="/works">
+                <a>Works</a>
+              </Link>
+            </li>
+            <li className="header_nav_list_item">
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li className="header_nav_list_item">
+              <Link href="/members">
+                <a>Members</a>
+              </Link>
+            </li>
+            <li className="header_nav_list_item">
+              <Link href="/careers">
+                <a>Careers</a>
+              </Link>
+            </li>
+            <li className="header_nav_list_item">
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       {children}
       {(() => {
         if (router.pathname !== '/') {
-          return <Footer />;
+          return (
+            /*<Footer />*/
+            <footer className="footer">
+              <Link href="/">
+                <a className="footer_link">
+                  <img
+                    src="/assets/img/common/footer-logo.svg"
+                    className="footer_logo"
+                    width="132"
+                    height="49"
+                    alt="test. inc"
+                  />
+                </a>
+              </Link>
+              <nav className="footer_nav">
+                <ul className="footer_nav_list">
+                  <li className="footer_nav_list_item">
+                    <Link href="/works">
+                      <a>Works</a>
+                    </Link>
+                  </li>
+                  <li className="footer_nav_list_item">
+                    <Link href="/about">
+                      <a>About</a>
+                    </Link>
+                  </li>
+                  <li className="footer_nav_list_item">
+                    <Link href="/members">
+                      <a>Members</a>
+                    </Link>
+                  </li>
+                  <li className="footer_nav_list_item">
+                    <Link href="/careers">
+                      <a>Careers</a>
+                    </Link>
+                  </li>
+                  <li className="footer_nav_list_item">
+                    <Link href="/contact">
+                      <a>Contact</a>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <a href="#body">pageTop</a>
+            </footer>
+          );
         }
       })()}
     </>
