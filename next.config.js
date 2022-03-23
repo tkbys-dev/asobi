44c0b5081144459b00e3b01756d24dbbbdf3b595
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-23 15:45:34 +0900

    update

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   basePath: buildEnv == 'production' ? '/' : '',
+  build: {
+    extend(config, ctx) {
+      let path = require('path');
+      config.resolve.alias['@/components'] = path.join(
+        __dirname,
+        'src/components'
+      );
+    },
+  },
 };
