2a70256da75c3a760a725f20474bf5a2474e6636
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-23 12:32:26 +0900

    mod path

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function Layout({ children }: any) {
   const router = useRouter();
