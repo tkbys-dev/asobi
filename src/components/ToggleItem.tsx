6244521b29c14215bc0e081363768c1f663b7e2d
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-29 12:18:39 +0900

    update style

--- src/components/ToggleItem.tsx
+++ src/components/ToggleItem.tsx
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0 },
+  open: { display: 'flex', opacity: 1 },
+  closed: { display: 'none', opacity: 0 },
 };
 
 export default function ToggleItem() {
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    return () => {
+      // document.body.style.overflow = 'hidden';
+    };
+  }, [isOpen]);
+
   return (
     <>
       <motion.a
+        variants={{
+          open: {
+            transition: {
+              staggeredChildren: 0.5,
+            },
+          },
+        }}
         animate={isOpen ? 'open' : 'closed'}
         className="header_btn"
         onClick={() => setIsOpen((isOpen) => !isOpen)}
@@ -50,32 +63,47 @@ export default function ToggleItem() {
       </motion.a>
       <motion.nav
         animate={isOpen ? 'open' : 'closed'}
-        initial={{ x: '-100%' }}
+        initial={{ opacity: 0 }}
         variants={variants}
         className="header_nav"
       >
         <ul className="header_nav_list">
-          <li className="header_nav_list_item">
+          <li
+            className="header_nav_list_item"
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+          >
             <Link href="/works">
               <a>Works</a>
             </Link>
           </li>
-          <li className="header_nav_list_item">
+          <li
+            className="header_nav_list_item"
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+          >
             <Link href="/about">
               <a>About</a>
             </Link>
           </li>
-          <li className="header_nav_list_item">
+          <li
+            className="header_nav_list_item"
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+          >
             <Link href="/members">
               <a>Members</a>
             </Link>
           </li>
-          <li className="header_nav_list_item">
+          <li
+            className="header_nav_list_item"
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+          >
             <Link href="/careers">
               <a>Careers</a>
             </Link>
           </li>
-          <li className="header_nav_list_item">
+          <li
+            className="header_nav_list_item"
+            onClick={() => setIsOpen((isOpen) => !isOpen)}
+          >
             <Link href="/contact">
               <a>Contact</a>
             </Link>
