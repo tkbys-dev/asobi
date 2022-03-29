1b8ece434f3b0c99ceb2cb6d4520ffaca0a44624
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-29 17:10:16 +0900

    set three-fiber

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from 'next';
+import React, { useRef, useState } from 'react';
+import THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+const DrawCanvas: NextPage = () => {
+  const Box = (props: JSX.IntrinsicElements['mesh']) => {
+    const ref = useRef<THREE.Mesh>(null!);
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+    useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    );
+  };
+
+  return (
+    <Canvas
+      style={{
+        width: 100 + 'vw',
+        height: 100 + 'vh',
+        // position: 'fixed',
+        // top: 0,
+        // left: 0,
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+};
+
+export default DrawCanvas;
---
+++ src/pages/sample.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import DrawCanvas from '@/components/Canvas';
+import type { NextPage } from 'next';
+
+const Sample: NextPage = () => {
+  return (
+    <>
+      <DrawCanvas />
+    </>
+  );
+};
+
+export default Sample;
