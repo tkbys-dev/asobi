d4a2787142c36efd144b22ae0d34fc7c49fd341b
t_kobayashi <t_kobayashi@MacBook-Pro.local>
2022-03-30 14:48:34 +0900

    update p5

--- src/p5/basep5.ts
+++ src/p5/basep5.ts
@@ -3,36 +3,79 @@ import type p5Type from 'p5';
 let x = 50;
 let y = 50;
 
-const canvasX = 1000;
-const canvasY = 500;
+const canvasX = 1500;
+const canvasY = 1000;
+
+let mass: number[] = [];
+let positionX: number[] = [];
+let positionY: number[] = [];
+let velocityX: number[] = [];
+let velocityY: number[] = [];
+
+function addNewParticle(p5: p5Type): void {
+  mass.push(p5.random(0.003, 0.03));
+  positionX.push(p5.mouseX);
+  positionY.push(p5.mouseY);
+  velocityX.push(0);
+  velocityY.push(0);
+}
+
+function mouseClicked() {
+  addNewParticle(p5);
+}
+
+function mouseDragged() {
+  addNewParticle(p5);
+}
 
 export const setup = (p5: p5Type, canvasParentRef: Element): void => {
-  p5.createCanvas(canvasX, canvasY, 'webgl').parent(canvasParentRef);
+  p5.createCanvas(p5.windowWidth, p5.windowHeight, 'webgl').parent(
+    canvasParentRef
+  );
+  p5.noStroke();
+  p5.fill(64, 255, 255, 192);
 };
 
 export const draw = (p5: p5Type): void => {
-  // p5.setup = () => {
-  //   p5.createCanvas(720, 400, "webgl");
-  // };
-
   p5.draw = () => {
-    p5.background(0);
-    p5.rotateY(p5.frameCount * 0.01);
+    addNewParticle(p5);
 
-    for (let j = 0; j < 5; j++) {
-      p5.push();
-      for (let i = 0; i < 80; i++) {
-        p5.translate(
-          p5.sin(p5.frameCount * 0.001 + j) * 100,
-          p5.sin(p5.frameCount * 0.001 + j) * 100,
-          i * 0.1
-        );
-        p5.rotateZ(p5.frameCount * 0.002);
-        p5.push();
-        p5.sphere(8, 6, 4);
-        p5.pop();
+    p5.background(32);
+
+    for (var particleA = 0; particleA < mass.length; particleA++) {
+      var accelerationX = 0,
+        accelerationY = 0;
+
+      for (var particleB = 0; particleB < mass.length; particleB++) {
+        if (particleA != particleB) {
+          var distanceX = positionX[particleB] - positionX[particleA];
+          var distanceY = positionY[particleB] - positionY[particleA];
+
+          var distance = p5.sqrt(distanceX * distanceX + distanceY * distanceY);
+          if (distance < 1) distance = 1;
+
+          var force = ((distance - 320) * mass[particleB]) / distance;
+          accelerationX += force * distanceX;
+          accelerationY += force * distanceY;
+        }
       }
-      p5.pop();
+
+      velocityX[particleA] =
+        velocityX[particleA] * 0.99 + accelerationX * mass[particleA];
+      velocityY[particleA] =
+        velocityY[particleA] * 0.99 + accelerationY * mass[particleA];
+    }
+
+    for (var particle = 0; particle < mass.length; particle++) {
+      positionX[particle] += velocityX[particle];
+      positionY[particle] += velocityY[particle];
+
+      p5.ellipse(
+        positionX[particle],
+        positionY[particle],
+        mass[particle] * 1000,
+        mass[particle] * 1000
+      );
     }
   };
 };
